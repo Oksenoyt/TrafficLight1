9ba0ca3698a7fdf2ecfdcb09d9b1f59a2b561cd8
Oksenoyt <a.oksenoyt@gmail.com>
2022-06-17 02:17:40 +0300

    Created views and button

--- TrafficLight1/ViewController.swift
+++ TrafficLight1/ViewController.swift
@@ -10,12 +10,15 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var firstLight: UIView!
+    @IBOutlet var secondLight: UIView!
+    @IBOutlet var thirdLight: UIView!
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         firstLight.layer.cornerRadius = 63
-        
+        secondLight.layer.cornerRadius = 63
+        thirdLight.layer.cornerRadius = 63
     }
 
 
