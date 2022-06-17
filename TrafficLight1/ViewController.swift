7b1eca270a90a9efaf740fad8aec79e98b252298
Oksenoyt <a.oksenoyt@gmail.com>
2022-06-17 04:16:48 +0300

    Button name changes firstLight switches its alpha

--- TrafficLight1/ViewController.swift
+++ TrafficLight1/ViewController.swift
@@ -14,13 +14,26 @@ class ViewController: UIViewController {
     @IBOutlet var thirdLight: UIView!
     @IBOutlet var startButton: UIButton!
     
+    var firstStart = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        firstLight.layer.cornerRadius = 63
-        secondLight.layer.cornerRadius = 63
-        thirdLight.layer.cornerRadius = 63
+        firstLight.layer.cornerRadius = 90
+        secondLight.layer.cornerRadius = 90
+        thirdLight.layer.cornerRadius = 90
+        startButton.layer.cornerRadius = 10
+        firstLight.alpha = 0.2
+        secondLight.alpha = 0.2
+        thirdLight.alpha = 0.2
     }
-
-
+    
+    @IBAction func startButtonTapped() {
+        if firstStart {
+            firstStart = false
+            startButton.setTitle("Next", for: .normal)
+            firstLight.alpha = 1
+        }
+    }
+    
 }
 
